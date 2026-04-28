72a0d319cf93d99486c174703074499cc57f984f
Car_Bit <olegkorbovskij@gmail.com>
2026-04-28 06:40:10 +0300

    добавление элемента в массив вручную

---
+++ src/task2.py
@@ -0,0 +1,22 @@
+# добавление элемента массива в ручную
+
+class SimpleArray:
+    def __init__(self):
+        self.capacity  = 5 
+        self.data = [None]* self.capacity
+
+    def set(self,index, value):
+        self.data[index]= value
+
+    def get(self, index):
+        return self.data[index]
+
+
+
+sa = SimpleArray()
+sa.set(1,'A')
+sa.set(2, 'D')
+sa.set(3, 'C')
+sa.set(4, 'B')
+
+print(sa.get(4))
