061705e5240ffc13370ee37b899ecc70eb510a66
Car_Bit <olegkorbovskij@gmail.com>
2026-04-29 11:10:33 +0300

    добавил функцию resize, вынес логику из append в resize

--- src/task4.py
+++ src/task4.py
@@ -1,33 +1,51 @@
 # создание динамического массива с ручным выделение памяти
 
 class DynamicArray:
-    def __init__(self):
-        self.capacity = 5
+    def __init__(self, capacity = 5):
+        self.initial_capacity = capacity
+        self.capacity = self.initial_capacity
         self.data = [None]*self.capacity
         self.length =0
+    
+    def resize(self,new_capacity):
+        if new_capacity == self.capacity:
+            return
+        if new_capacity <self.length:
+            raise ValueError(f"размер new_capacity меньше чем длина массива")
+
+            
+        new_data = [None]*new_capacity
+        for i in range(self.length):
+            new_data[i] = self.data[i]
+        self.data = new_data
+        self.capacity = new_capacity
+
 
     def append(self, value):
-        if (self.capacity == self.length):
-            self.capacity *=2
-            
-            new_data = [None]*self.capacity
-            for i in range(self.length):
-                new_data[i] = self.data[i]
-            self.data = new_data
+        if self.capacity ==self.length:
+            self.resize(self.capacity*2)
 
 
         self.data[self.length] = value
         self.length +=1
 
+    def reset(self):
+        self.data = [None]* self.initial_capacity
+        self.length =0 
+
 da = DynamicArray()
-da.append('a')
-da.append('b')
-da.append('c')
-da.append('d')
-da.append('e')
-da.append('f')
-da.append('g')
-da.append('h')
-da.append('i')
+for x in 'abcdefckfjsd': da.append(x)
 
 print(da.data)
+#da.reset()
+print(da.data)
+da.resize(11)
+print(da.data)
+
+
+
+
+
+
+
+
